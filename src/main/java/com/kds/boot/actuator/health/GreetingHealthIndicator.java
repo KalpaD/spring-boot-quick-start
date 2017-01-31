ca7926f2420bacdbb1cdb58e5e175f0ccdc1fb4c
kalpads <senanayake.kalpa@gmail.com>
2017-01-31 22:19:03 +1100

    Using spring boot actuator and demo how to customize the actuator health check

---
+++ src/main/java/com/kds/boot/actuator/health/GreetingHealthIndicator.java
@@ -0,0 +1,36 @@
+package com.kds.boot.actuator.health;
+
+import com.kds.boot.service.GreetingService;
+import com.kds.boot.web.model.Greeting;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.stereotype.Component;
+
+import java.util.Collection;
+
+/**
+ * Created by KDS on 1/31/2017.
+ */
+
+/**
+ * Demonstrate how to customize the spring boot actuator featurs to provide
+ * customized HealthIndicator
+ */
+@Component
+public class GreetingHealthIndicator implements HealthIndicator{
+
+    @Autowired
+    private GreetingService greetingService;
+
+    @Override
+    public Health health() {
+        Collection<Greeting> greetings = greetingService.findAll();
+
+        if (greetings == null || greetings.size() == 0) {
+            return Health.down().withDetail("count", 0).build();
+        }
+
+        return Health.up().withDetail("count", greetings.size()).build();
+    }
+}
